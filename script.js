c16bebd6a29119f82a336a7c2a1ec1874342757b
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-04-21 15:40:09 +0800

    Add explanatory comments

--- script.js
+++ script.js
@@ -9,14 +9,21 @@ for (let i = 1; i <= 100; i++) {
 
     // Runs if the the value of i is divisible by 3 and 5 
     if (divisibleByThree == 0 && divisibleByFive == 0) {
+        // Replaces value to Fizz Buzz
         console.log('Fizz Buzz')
     }
     // Runs if the value of i is only divisible by 3
     else if (divisibleByThree == 0) {
+        // Replaces values to Fizz
         console.log('Fizz')
     }
     // Runs if the value of i is only divisible by 5
     else if (divisibleByFive == 0) {
+        // Replaces value to Buzz
         console.log('Buzz')
     }
+    else (
+        // Simply prints out the value of i
+        console.log(i)
+    )
 }
