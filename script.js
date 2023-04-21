fa9e094698b021bdc67c2b966e476d9976326502
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-04-21 15:36:17 +0800

    Create conditionals for divisibility by 3 and 5

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ for (let i = 1; i <= 100; i++) {
     let divisibleByThree = i % 3;
     // Calculates the remainder of i value and 5
     let divisibleByFive = i % 5;
+
+    // Runs if the the value of i is divisible by 3 and 5 
+    if (divisibleByThree == 0 && divisibleByFive == 0) {
+        console.log('Fizz Buzz')
+    }
 }
