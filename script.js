692c86dab649fa167c2ba00375071129f2cd7f5c
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-04-21 15:33:19 +0800

    Create modulo variables

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
 // Prints a loop of 1 - 100
 for (let i = 1; i <= 100; i++) {
     
+    // Calculates the remainder of i value and 3
+    let divisibleByThree = i % 3;
+    // Calculates the remainder of i value and 5
+    let divisibleByFive = i % 5;
 }
